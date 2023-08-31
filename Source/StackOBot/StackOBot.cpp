352a99f0012ba29e6a5a4f31543cbd71ba6a458c
crazytuzi <957824770@qq.com>
2023-08-31 19:32:51 +0800

    Convert to C++ project

---
+++ Source/StackOBot/StackOBot.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StackOBot.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, StackOBot, "StackOBot");
