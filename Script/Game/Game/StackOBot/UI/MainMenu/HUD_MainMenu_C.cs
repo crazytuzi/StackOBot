10856e22229cb8f73f396f6bee4c5fece44a05eb
crazytuzi <957824770@qq.com>
2023-09-05 00:30:55 +0800

    HUD_MainMenu

---
+++ Script/Game/Game/StackOBot/UI/MainMenu/HUD_MainMenu_C.cs
@@ -0,0 +1,104 @@
+using Script.Common;
+using Script.Engine;
+using Script.Game.StackOBot.Blueprints.Framework;
+using Script.Game.StackOBot.UI.LoadingScreen;
+using Script.Library;
+using Script.UMG;
+
+namespace Script.Game.StackOBot.UI.MainMenu
+{
+    [IsOverride]
+    public partial class HUD_MainMenu_C
+    {
+        [IsOverride]
+        public override void ReceiveBeginPlay()
+        {
+            AddLoadingScreenWidget();
+
+            AddMainMenuWidget();
+
+            PlayMusic();
+        }
+
+        [IsOverride]
+        public override void ReceiveEndPlay(EEndPlayReason EndPlayReason)
+        {
+            MyLoadingWidget?.DoneFadingEvent.Clear();
+        }
+
+        private void AddLoadingScreenWidget()
+        {
+            MyLoadingWidget = Unreal.NewObject<BPW_LoadingScreen_C>(this);
+
+            MyLoadingWidget.SetFade();
+
+            MyLoadingWidget.AddToViewport(90000);
+        }
+
+        private void AddMainMenuWidget()
+        {
+            MyMainMenu = Unreal.NewObject<BPW_MainMenu_C>(this);
+
+            MyMainMenu.AddToViewport();
+
+            var PlayerController = GetOwningPlayerController();
+
+            UWidgetBlueprintLibrary.SetInputMode_GameAndUIEx(GetOwningPlayerController(), MyMainMenu,
+                EMouseLockMode.DoNotLock, false);
+
+            PlayerController.bShowMouseCursor = true;
+
+            var PlayerPawn = UGameplayStatics.GetPlayerPawn(this, 0);
+
+            PlayerPawn.DisableInput(PlayerController);
+        }
+
+        /*
+         * Start the music that is handled in the game instance to have a persitent music between level loads.
+         */
+        private void PlayMusic()
+        {
+            (UGameplayStatics.GetGameInstance(this) as IBPI_GameInstance_C)?.PlayMusic(0.6);
+        }
+
+        /*
+         * Quit (Interface Event, called by Main Menu Widget)
+         */
+        [IsOverride]
+        public void QuitGame()
+        {
+            var PlayerController = GetOwningPlayerController();
+
+            UKismetSystemLibrary.QuitGame(this, PlayerController, EQuitPreference.Quit, false);
+        }
+
+        /*
+         * Load Game (Interface Event, called by Main Menu Widget)
+         */
+        [IsOverride]
+        public void LoadGame()
+        {
+            if (!StartLevelName.Equals("None"))
+            {
+                if (MyLoadingWidget != null)
+                {
+                    MyLoadingWidget.SetVisibility(ESlateVisibility.Visible);
+
+                    MyLoadingWidget.SetFade(1.0);
+
+                    MyLoadingWidget.DoneFadingEvent.Add(this, LoadingDoneFading);
+                }
+            }
+            else
+            {
+                UKismetSystemLibrary.PrintString(this,
+                    "ERROR: No StartLevelName defined in the HUD class. Please set this to your game's first Level name.");
+            }
+        }
+
+        private void LoadingDoneFading()
+        {
+            UGameplayStatics.OpenLevel(this, StartLevelName);
+        }
+    }
+}
