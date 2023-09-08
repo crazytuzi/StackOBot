8f31d9d0d2acce6da1cfc9dc4d67d917c28afb8a
crazytuzi <957824770@qq.com>
2023-09-08 20:17:36 +0800

    BPW_Button_Base_C

---
+++ Script/Game/Game/StackOBot/UI/PauseMenu/BPW_Button_Base_C.cs
@@ -0,0 +1,18 @@
+﻿using Script.Common;
+
+namespace Script.Game.StackOBot.UI.PauseMenu
+{
+    [IsOverride]
+    public partial class BPW_Button_Base_C
+    {
+        /*
+         * This button is used on all menus so we can implement a consistend look and behaviour.
+         * The label string is exposed and the menus can register to the OnClick event to implement the behaviour.
+         */
+        [IsOverride]
+        public override void PreConstruct(bool IsDesignTime)
+        {
+            Label.SetText(Label_String);
+        }
+    }
+}
