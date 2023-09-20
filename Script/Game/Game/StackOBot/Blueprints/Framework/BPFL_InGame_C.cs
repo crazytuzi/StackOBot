06fa0c6222027930d3524af8885f644884b54d81
crazytuzi <957824770@qq.com>
2023-09-20 14:03:49 +0800

    BPFL_InGame_C

---
+++ Script/Game/Game/StackOBot/Blueprints/Framework/BPFL_InGame_C.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Script.Common;
+using Script.CoreUObject;
+
+namespace Script.Game.StackOBot.Blueprints.Framework
+{
+    [IsOverride]
+    public partial class BPFL_InGame_C
+    {
+        [IsOverride]
+        public static void GetMaxBots(UObject __WorldContext, out Int32 Max)
+        {
+            Max = 8;
+        }
+    }
+}
